5bc4e557f63e616204487270555ce5e3029df69f
Cléber Severo <clebersevero1998@gmail.com>
2024-12-02 22:25:10 -0300

    feat: config page added

---
+++ src/components/paginas/Configuracao.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Formulario from '../Formulario'
+import ListaParticipantes from '../ListaParticipantes'
+import Rodape from '../Rodape'
+
+function Configuracao() {
+
+  return (
+    <>
+      <Formulario /> 
+      <ListaParticipantes /> 
+      <Rodape /> 
+    </>  
+  )
+}
+
+export default Configuracao
