e70719952d63f8d4b895f1c729f6115860495fa9
Cléber Severo <clebersevero1998@gmail.com>
2024-12-05 00:26:51 -0300

    style: form style

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,39 +1,44 @@
-import React, { useRef, useState } from 'react'
-import { useAdicionarParticipante } from '../state/hooks/useAdicionarParticipante'
-import { useMensagemDeErro } from '../state/hooks/useMensagemDeErro'
+import React, { useRef, useState } from "react";
+import { useAdicionarParticipante } from "../state/hooks/useAdicionarParticipante";
+import { useMensagemDeErro } from "../state/hooks/useMensagemDeErro";
 
-import './styles/Formulario.css'
+import "./styles/Formulario.css";
+import ProfileIcon from "../assets/ProfileIcon";
 
 function Formulario() {
-  const [nome, setNome] = useState('')
+  const [nome, setNome] = useState("");
 
-  const inputRef = useRef<HTMLInputElement>(null)
-  const adicionarNaLista = useAdicionarParticipante()
+  const inputRef = useRef<HTMLInputElement>(null);
+  const adicionarNaLista = useAdicionarParticipante();
 
-  const mensagemDeErro = useMensagemDeErro()
+  const mensagemDeErro = useMensagemDeErro();
 
   const adicionarParticipante = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    adicionarNaLista(nome)
+    event.preventDefault();
+    adicionarNaLista(nome);
 
-    setNome('')
-    inputRef.current?.focus()
-  }
+    setNome("");
+    inputRef.current?.focus();
+  };
 
   return (
-    <form onSubmit={adicionarParticipante} className='form-amigo-secreto'>
-      <input
-        ref={inputRef}
-        value={nome}
-        onChange={e => setNome(e.target.value)}
-        type="text" 
-        placeholder='Insira os nomes dos participantes' 
-      />
-     
-      <button disabled={!nome} >Adicionar</button>
-      { mensagemDeErro && <p role='alert' >{mensagemDeErro}</p>}
+    <form onSubmit={adicionarParticipante} className="form-amigo-secreto">
+      <h2>Vamos começar!</h2>
+      <div>
+        <ProfileIcon />
+        <input
+          ref={inputRef}
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          type="text"
+          placeholder="Insira os nomes dos participantes"
+        />
+
+        <button disabled={!nome}>Adicionar</button>
+      </div>
+      {mensagemDeErro && <p role="alert">{mensagemDeErro}</p>}
     </form>
-  )
+  );
 }
 
-export default Formulario
+export default Formulario;
