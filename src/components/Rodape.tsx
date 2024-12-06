0aac0872fdec3ad236262fdb22656d6059e97de8
Cléber <clebersevero1998@gmail.com>
2024-12-06 14:08:06 -0300

    feat: teat if sort is being called

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -3,13 +3,18 @@ import { useListaParticipantes } from '../state/hooks/useListaDeParticipantes'
 import { useNavigate } from 'react-router-dom'
 import CustomBtn from './CustomBtn'
 import PlayIcon from '../assets/PlayIcon'
+import { useSorteador } from '../state/hooks/useSorteador'
 
 const Rodape = () => {
   const participantes = useListaParticipantes()
   
   const navegarPara = useNavigate()
 
+  const sortear = useSorteador()
+
   const iniciar = () => {
+    sortear()
+
     navegarPara('/sorteio')
   }
   
