45f869a5ba79373abf4772b55ce92f6acc4fad65
Cléber Severo <clebersevero1998@gmail.com>
2024-12-08 00:28:20 -0300

    fix: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { BrowserRouter, Navigate, Route, RouterProvider, Routes } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
-import Configuracao from './components/paginas/Configuracao'
-import Sorteio from './components/Sorteio'
-import Home from './components/paginas/Home'
 import { router } from './routes/routes'
 
 function App() {
